870582ed0d4dbc4e73c78c6dae527e7038d5fa83
hack86bx <arpacie300@gmail.com>
2024-04-28 20:31:55 +0200

    280424

--- controller/adminController.php
+++ controller/adminController.php
@@ -1,7 +1,6 @@
 <?php
 
 // si on veut se déconnecter
-
 if (isset($_GET['disconnect'])) {
     // on se déconnecte
     disconnectAdministrator();
@@ -12,10 +11,13 @@ if (isset($_GET['disconnect'])) {
 //Si on veut créer un lieu
 if (isset($_GET['create'])) {
 
-    //Si on a cliqué sur insérer
+    /*Si on a cliqué sur insérer et Si on veut créer un lieu :
+    On récupère les données du formulaire
+    On insère les données dans la base de données
+    Si l'insertion réussit, on redirige vers la page d'accueil*/
     if (isset(
-        $_POST['title'],
-        $_POST['geolocdesc'],
+        $_POST['title'],   
+        $_POST['geolocdesc'], 
         $_POST['latitude'],
         $_POST['longitude']
     )) {
@@ -31,6 +33,17 @@ if (isset($_GET['create'])) {
             exit();
         }
     }
+
+
+   /* isset() : Cette fonction vérifie si une ou plusieurs variables sont définies et non nulles, elle vérifie si les variables $_POST['title'], $_POST['geolocdesc'], $_POST['latitude'] et $_POST['longitude'] existent et ont une valeur.
+$_POST : C’est un tableau associatif en PHP qui contient les données envoyées par un formulaire HTML via la méthode POST.Vous récupérez les valeurs des champs de formulaire nommés title, geolocdesc, latitude et longitude.
+
+htmlspecialchars() : Cette fonction convertit les caractères spéciaux en entités HTML. Elle est utilisée ici pour sécuriser les données entrées par l’utilisateur et éviter les attaques XSS (cross-site scripting). 
+Par exemple, si l’utilisateur entre <script>alert('Hello');</script>, cette fonction le convertira en &lt;script&gt;alert('Hello');&lt;/script&gt;.
+
+strip_tags() : Cette fonction supprime toutes les balises HTML et PHP d’une chaîne de caractères. Elle est utilisée ici pour s’assurer que les valeurs des champs ne contiennent pas de balises potentiellement dangereuses.
+Conversion en nombre flottant : Les variables $latitude et $longitude sont converties en nombres flottants (décimaux) à l’aide de (float) pour s’assurer qu’elles contiennent des valeurs numériques.
+Insertion dans la base de données : Enfin, les valeurs nettoyées et converties sont utilisées pour insérer un enregistrement dans la base de données à l’aide de la fonction insertOneGeolocById($db, $title, $geolocdesc, $latitude, $longitude). Si l’insertion réussit, l’utilisateur est redirigé vers la page d’accueil.*/
 
     //chargement de la vue
     include "../view/admin/admin.insert.view.html.php";
